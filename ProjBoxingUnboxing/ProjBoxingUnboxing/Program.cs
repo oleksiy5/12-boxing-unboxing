0e77ece30897f594470204d5e8198baca35d13cd
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-22 19:07:01 +0200

    f

---
+++ ProjBoxingUnboxing/ProjBoxingUnboxing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjBoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object obj = "2017-01-01";
+            DateTime date = (DateTime)obj;
+
+
+            ///*
+            // * Przykład Boxing (pakowanie)
+            // */
+            //int number = 1007;  //<-- value type
+            //object obj = number;//<--reference type
+
+            ///*
+            // * Przykład Unboxing (wypakowanie)
+            // */
+            //object obj2 = "1007";   //<--reference type
+            //int number2 = (int)obj; //<-- value type
+
+            
+
+
+        }
+    }
+}
